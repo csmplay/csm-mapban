6fd5830d42191a20566b53803028fd9e95f62624
ch4og <mitanick@ya.ru>
2024-12-05 04:02:48 +0300

    fix images scaling

--- apps/frontend/src/components/ui/ban.tsx
+++ apps/frontend/src/components/ui/ban.tsx
@@ -54,7 +54,7 @@ export default function AnimatedPickCard({teamName, mapName}: AnimatedBanCardPro
                                 alt={mapName}
                                 draggable={false}
                                 fill
-                                object-fit="cover"
+                                objectFit="cover"
                                 sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
                                 style={{
                                     clipPath: 'polygon(0% 50%, 20% 0%, 100% 0%, 100% 50%, 80% 100%, 0% 100%)'
